4211eb10497664ffae893abee6afc42a0c70e89a
srividya-p <srividya.ssa@gmail.com>
2021-04-25 17:53:24 +0530

    Menu + Load Roads

--- Places/load.py
+++ Places/load.py
@@ -1,6 +1,7 @@
 from qgis.core import *
+from PyQt5.QtGui import *
 
-def load_layers():
+def load_places_layers():
     base_layer = QgsVectorLayer(
         '/home/pika/Desktop/QGIS/Shape-Files/Base-Map/IND_adm2.shp', "Base Map", "ogr")
     places_layer = QgsVectorLayer(
@@ -20,4 +21,16 @@ def load_layers():
             {'name': 'circle', 'color': 'green'})
         places_layer.renderer().setSymbol(symbol)
         places_layer.triggerRepaint()
+
+def load_path_layers():
+    load_places_layers()
+    roads_layer = QgsVectorLayer(
+        '/home/pika/Desktop/QGIS/Shape-Files/Places/Roads.gpkg', "Roads", "ogr")
+    
+    if(not QgsProject.instance().mapLayersByName('Roads')):
+        roads_layer.renderer().symbol().setColor(QColor("brown"))
+        roads_layer.triggerRepaint()
+        QgsProject.instance().addMapLayer(roads_layer)
+        print('Roads Layer added to Registry!')
+
 
--- Places/main.py
+++ Places/main.py
@@ -1,5 +1,7 @@
 import importlib.util
 from qgis.core import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 approot = QgsProject.instance().homePath()
 
@@ -11,8 +13,21 @@ circle_spec = importlib.util.spec_from_file_location("draw_circle", approot+"/dr
 draw_circle_file = importlib.util.module_from_spec(circle_spec)
 circle_spec.loader.exec_module(draw_circle_file)
 
-load_file.load_layers()
-canvas_clicked = draw_circle_file.DrawSectorCircle(iface.mapCanvas(), iface)
-iface.mapCanvas().setMapTool(canvas_clicked)
-iface.messageBar().pushMessage("Welcome",
-                               "Select a Center Point.\nPress 'Q' to Quit.", level=Qgis.Info, duration=3)
+msgbox = QMessageBox()
+msgbox.setWindowTitle('Welcome to TaskGIS')
+msgbox.setStandardButtons(QMessageBox.Close)
+msgbox.setText(' Choose the Task to be performed')
+places_button = msgbox.addButton('Query Places', QMessageBox.AcceptRole)
+path_button = msgbox.addButton('Shortest Path', QMessageBox.AcceptRole)
+
+ret = msgbox.exec()
+
+if ret == QMessageBox.Close:
+    QCloseEvent()
+elif msgbox.clickedButton() is places_button:
+    load_file.load_places_layers()
+    canvas_clicked = draw_circle_file.DrawSectorCircle(iface.mapCanvas(), iface)
+    iface.mapCanvas().setMapTool(canvas_clicked)
+    iface.messageBar().pushMessage("Welcome", "Select a Center Point.\nPress 'Q' to Quit.", level=Qgis.Info, duration=3)
+elif msgbox.clickedButton() is path_button:
+    load_file.load_path_layers()
